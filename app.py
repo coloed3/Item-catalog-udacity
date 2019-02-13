6c70d9ba20a5ad59f4ec5e3f7dcec9a4e9d141eb
Eddie Colon <coloed3@gmail.com>
2019-02-12 23:26:48 -0700

    added the login page logic, and added the back end logic for the /gconnect, created new google oauth account added and testest

--- app.py
+++ app.py
@@ -21,9 +21,10 @@ client_secrets={}
 """======================================================================="""
 def load_json_secrets():
     global client_secrets
-    client_secrets = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+    client_secrets = json.load(open('client_secrets.json'))['web']
 
 """=========================================================================="""
+
 app = Flask(__name__)
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
@@ -93,20 +94,97 @@ def showLogin():
     else:
         return render_template('loginin.html')
 
-
 # google+ oauth login route
-@app.route('/gconnect', methods=['GET', 'POST'])
+@app.route('/gconnect', methods=['POST'])
 def gconnect():
-    pass
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Obtain authorization code
+    code = request.data
+
+    try:
+
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secret.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID didn't match the given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != client_secrets['client_id']:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print("Token's client ID does not match app's.")
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    stored_credentials = AccessTokenCredentials(
+        login_session.get('access_token'), 'user-agent-value')
+
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_credentials is not None and gplus_id == stored_gplus_id:
+        response = make_response(
+            json.dumps('Current user is already connected.'),
+            200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+    # ADD PROVIDER TO LOGIN SESSION
+    login_session['provider'] = 'google'
+
+    # see if user exists, if it doesn't make a new one
+    user_id = getUserID(data["email"])
+    if not user_id:
+        user_id = createUser(login_session)
+        login_session['user_id'] = user_id
+
+    return 'logged in'
     # will be using code used in the last project to brign over
     # will need to make sure the json files are working as intended
 
 
-# gdisconnect    # with op
-# @app.route('/gdisconne    # with opct')
-# def gdisconnect():
-#     pass
-    # will use code from our last project  and will route to logout template
+
 
 
 # route fdor seeing all our items
@@ -131,7 +209,29 @@ def edit_item(item_name):
 def delete_item(item_name):
     return render_template('deleteitem.html')
 
+"""=========================================================================
+code below taken from the restaurant application. 
+==========================================================================="""
+def createUser(login_session):
+    newUser = User(name=login_session['username'], email=login_session[
+                   'email'], picture=login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
 
+
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id=user_id).one()
+    return user
+
+
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
 
 # ====== took this from our crud course for creating function for actions we need to preform
 def is_logged_in():
@@ -143,7 +243,11 @@ def getToken():
     return ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in xrange(32))
 
+
 if __name__ == "__main__":
+    """stackoverflow.com/questions/26080872/secret-key-not-set-in-flask-session-using-the-flask-session-extension,
+    had issues with errors when attempting to login in to session"""
+    app.secret_key = 'super_secret_key'
     load_json_secrets()
     app.debug = True
     app.run(host='0.0.0.0', port=8000)
